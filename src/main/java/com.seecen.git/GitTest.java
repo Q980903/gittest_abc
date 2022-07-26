09f0ff4ac484a6cb1742534fb0aa858d82a6f6e3
huqihua <1411241573@qq.com>
2022-07-26 17:06:36 +0800

    second commit

---
+++ src/main/java/com.seecen.git/GitTest.java
@@ -0,0 +1,14 @@
+package com.seecen.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author Huqihua
+ * @create 2022/7/26 - 17:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+
+    }
+}
