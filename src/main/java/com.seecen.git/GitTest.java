e595b05e05895aad68f5489fb9f1330e9b2e266f
huqihua <1411241573@qq.com>
2022-07-26 17:12:58 +0800

    third commit

--- src/main/java/com.seecen.git/GitTest.java
+++ src/main/java/com.seecen.git/GitTest.java
@@ -9,6 +9,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
-
+        System.out.println("hello git2");
     }
 }
